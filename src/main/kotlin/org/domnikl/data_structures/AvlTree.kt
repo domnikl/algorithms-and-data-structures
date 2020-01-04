241272db415c89085367de1c295d8e0ab614ba6b
Dominik Liebler <liebler.dominik@gmail.com>
2020-01-04 20:40:59 +0100

    delete from AVL tree

--- src/main/kotlin/org/domnikl/data_structures/AvlTree.kt
+++ src/main/kotlin/org/domnikl/data_structures/AvlTree.kt
@@ -2,23 +2,29 @@ package org.domnikl.data_structures
 
 import kotlin.math.max
 
-class AvlTree<K : Comparable<K>,V> {
+class AvlTree<K : Comparable<K>,V>(vararg init: Pair<K,V>) {
     private var root: Node<K,V>? = null
+
+    init {
+        init.forEach {
+            insert(it.first, it.second)
+        }
+    }
 
     fun insert(key: K, value: V) {
         root = add(root, Node(key, value, 1))
     }
 
-    fun search(key: K, visited: ((K) -> Unit)? = null): V {
-        return search(key, root, visited)
+    fun search(key: K, visited: ((K) -> Unit)? = null): V? {
+        return search(key, root) { visited?.invoke(it.key) }?.value
     }
 
-    private fun search(key: K, node: Node<K,V>?, visited: ((K) -> Unit)? = null): V {
+    private fun search(key: K, node: Node<K,V>?, visited: ((Node<K,V>) -> Unit)? = null): Node<K,V>? {
         if (node == null) {
-            throw IndexOutOfBoundsException("Could not find key '${key}'")
+            return null
         }
 
-        visited?.let { it(node.key) }
+        visited?.let { it(node) }
 
         if (key < node.key) {
             return search(key, node.left, visited)
@@ -26,7 +32,7 @@ class AvlTree<K : Comparable<K>,V> {
             return search(key, node.right, visited)
         }
 
-        return node.value
+        return node
     }
 
     private fun add(node: Node<K,V>?, newNode: Node<K,V>): Node<K,V>? {
@@ -78,6 +84,45 @@ class AvlTree<K : Comparable<K>,V> {
         return node
     }
 
+    fun delete(key: K) {
+        delete(key, root)
+    }
+
+    private fun delete(key: K, tree: Node<K,V>?) {
+        if (tree == null) {
+            return
+        }
+
+        val visited = mutableListOf<Node<K,V>>()
+        val node = search(key, tree) {
+            visited.add(it)
+        } ?: return
+
+        val parent = when {
+            visited.size >= 2 -> visited.elementAt(visited.size - 2)
+            node == tree -> tree
+            else -> null
+        }
+
+        if (node.left == null && node.right == null) {
+            if (node == root && tree == root) root = null else parent?.removeChild(node)
+        } else if (node.left == null) {
+            if (node == root && tree == root) root = node.right else parent?.replaceChild(node, node.right)
+        } else if (node.right == null) {
+            if (node == root && tree == root) root = node.left else parent?.replaceChild(node, node.left)
+        } else {
+            val min = node.right!!.min()
+            val newNode = Node(min.key, min.value, node.height)
+            newNode.left = node.left
+            newNode.right = node.right
+            newNode.height = node.height - 1
+
+            if (node == root && tree == root) root = newNode else parent?.replaceChild(node, newNode)
+
+            delete(min.key, node.right)
+        }
+    }
+
     private class Node<K : Comparable<K>,V>(val key: K, val value: V, var height: Int) {
         var left: Node<K,V>? = null
         var right: Node<K,V>? = null
@@ -92,6 +137,8 @@ class AvlTree<K : Comparable<K>,V> {
             get() = leftHeight - rightHeight
 
         override fun toString() = "$key"
+
+        override fun equals(other: Any?) = if (other is Node<*,*>) other.key == key else false
 
         private fun updateHeight() {
             height = 1 + max(leftHeight, rightHeight)
@@ -113,10 +160,40 @@ class AvlTree<K : Comparable<K>,V> {
 
             other.right = this.also { it.left = other.right }
 
+            updateHeight()
             other.updateHeight()
-            this.updateHeight()
 
             return other
         }
+
+        fun removeChild(node: Node<K, V>) {
+            if (right == node) {
+                right = null
+            } else if (left == node) {
+                left = null
+            }
+        }
+
+        fun replaceChild(node: Node<K, V>, with: Node<K,V>?) {
+            if (right == node) {
+                right = with
+            } else if (left == node) {
+                left = with
+            }
+        }
+
+        fun min(): Node<K,V> {
+            return if (left == null) {
+                this
+            } else {
+                left!!.min()
+            }
+        }
+
+        override fun hashCode() = key.hashCode()
     }
+}
+
+fun <K : Comparable<K>,V> avlTree(items: Map<K,V>): AvlTree<K,V> {
+    return AvlTree(*items.map { Pair(it.key, it.value) }.toTypedArray())
 }
--- src/test/kotlin/org/domnikl/data_structures/AvlTreeTest.kt
+++ src/test/kotlin/org/domnikl/data_structures/AvlTreeTest.kt
@@ -1,6 +1,7 @@
 package org.domnikl.data_structures
 
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
 import org.junit.Test
 
 class AvlTreeTest {
@@ -33,7 +34,7 @@ class AvlTreeTest {
     }
 
     @Test
-    fun `will visit 20 nodes at max for 1000 elements`() {
+    fun `will visit 22 nodes at max for 1000 elements`() {
         repeat(1000) {
             val items = (0..1000).shuffled()
             val tree = AvlTree<Int, Int>().also { items.forEach { i -> it.insert(i, i) } }
@@ -46,19 +47,17 @@ class AvlTreeTest {
                     visited.add(it)
                 }
 
-                assert(visited.size <= 20) { "visited ${visited.size} nodes: ${visited.joinToString(", ")}" }
+                assert(visited.size <= 22) { "visited ${visited.size} nodes: ${visited.joinToString(", ")}" }
                 max = maxOf(max, visited.size)
             }
 
-            assert(max <= 20) { "visited $max nodes at max" }
+            assert(max <= 22) { "visited $max nodes at max" }
         }
     }
 
-    @Test(expected = IndexOutOfBoundsException::class)
-    fun `throws exception when element was not found`() {
-        val tree = AvlTree<Int, Int>()
-
-        tree.search(1)
+    @Test
+    fun `returns null when element was not found`() {
+        assertNull(AvlTree<Int, Int>().search(1))
     }
 
     @Test(expected = IllegalArgumentException::class)
@@ -67,5 +66,149 @@ class AvlTreeTest {
 
         tree.insert(1, 1)
         tree.insert(1, 1)
+    }
+
+    @Test
+    fun `can delete root node`() {
+        val tree = AvlTree<Int, String>()
+
+        tree.insert(1, "A")
+
+        assertEquals("A", tree.search(1))
+
+        tree.delete(1)
+
+        assertNull(tree.search(1))
+    }
+
+    @Test
+    fun `can delete node without children`() {
+        val tree = AvlTree<Int, String>()
+
+        tree.insert(2, "A")
+        tree.insert(1, "B")
+        tree.insert(3, "C")
+
+        assertEquals("C", tree.search(3))
+        tree.delete(3)
+        assertNull(tree.search(3))
+
+        assertEquals("B", tree.search(1))
+        tree.delete(1)
+        assertNull(tree.search(1))
+    }
+
+    @Test
+    fun `can delete node with left child`() {
+        val tree = AvlTree<Int, String>()
+
+        tree.insert(2, "A")
+        tree.insert(1, "B")
+
+        tree.delete(2)
+        assertNull(tree.search(2))
+        assertEquals("B", tree.search(1))
+    }
+
+    @Test
+    fun `can delete node with right child`() {
+        val tree = AvlTree<Int, String>()
+
+        tree.insert(2, "A")
+        tree.insert(3, "C")
+
+        tree.delete(2)
+        assertNull(tree.search(2))
+        assertEquals("C", tree.search(3))
+    }
+
+    @Test
+    fun `can delete node with two children`() {
+        val tree = AvlTree<Int, String>()
+
+        tree.insert(4, "A")
+        tree.insert(2, "B")
+        tree.insert(6, "C")
+        tree.insert(1, "D")
+        tree.insert(3, "E")
+        tree.insert(5, "F")
+        tree.insert(7, "G")
+
+        tree.delete(6)
+        assertNull(tree.search(6))
+        assertEquals("A", tree.search(4))
+        assertEquals("B", tree.search(2))
+        assertEquals("D", tree.search(1))
+        assertEquals("E", tree.search(3))
+        assertEquals("F", tree.search(5))
+        assertEquals("G", tree.search(7))
+    }
+
+    @Test
+    fun `can delete root node with two children`() {
+        val tree = AvlTree<Int, String>()
+
+        tree.insert(4, "A")
+        tree.insert(2, "B")
+        tree.insert(6, "C")
+        tree.insert(1, "D")
+        tree.insert(3, "E")
+        tree.insert(5, "F")
+        tree.insert(7, "G")
+
+        tree.delete(4)
+        assertNull(tree.search(4))
+        assertEquals("B", tree.search(2))
+        assertEquals("C", tree.search(6))
+        assertEquals("D", tree.search(1))
+        assertEquals("E", tree.search(3))
+        assertEquals("F", tree.search(5))
+        assertEquals("G", tree.search(7))
+    }
+
+    @Test
+    fun `can delete a single key and still find all other keys`() {
+        val items = (0..1000).shuffled()
+
+        for (item in items) {
+            val tree = avlTree(items.map { Pair(it, it) }.toMap())
+            val remainingItems = items.toMutableList() - item
+
+            tree.delete(item)
+
+            for (remaining in remainingItems) {
+                assertEquals(remaining, tree.search(remaining))
+            }
+        }
+    }
+
+    @Test
+    fun `can delete value with 1 child`() {
+        val items = mapOf(2 to 2, 1 to 1, 0 to 0, 4 to 4, 3 to 3, 5 to 5)
+        val tree = avlTree(items)
+
+        tree.delete(4)
+
+        assertEquals(0, tree.search(0))
+        assertEquals(1, tree.search(1))
+        assertEquals(2, tree.search(2))
+        assertEquals(3, tree.search(3))
+        assertEquals(5, tree.search(5))
+        assertNull(tree.search(4))
+    }
+
+    @Test
+    fun `can delete value with 2 children`() {
+        val items = mapOf(2 to 2, 1 to 1, 0 to 0, 4 to 4, 3 to 3, 5 to 5)
+        val tree = avlTree(items)
+
+        tree.delete(1)
+
+        assertEquals(0, tree.search(0))
+        assertEquals(4, tree.search(4))
+        assertEquals(2, tree.search(2))
+        assertEquals(3, tree.search(3))
+        assertEquals(5, tree.search(5))
+        assertNull(tree.search(1))
     }
 }
