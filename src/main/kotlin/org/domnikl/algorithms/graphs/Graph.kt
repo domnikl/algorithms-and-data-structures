67de0455cb4e93f0d27a0e6177de4b95ab183fbc
Dominik Liebler <liebler.dominik@gmail.com>
2020-01-06 00:04:52 +0100

    implemented depth-first-search for graph

--- src/main/kotlin/org/domnikl/algorithms/graphs/Graph.kt
+++ src/main/kotlin/org/domnikl/algorithms/graphs/Graph.kt
@@ -2,13 +2,14 @@ package org.domnikl.algorithms.graphs
 
 import org.domnikl.data_structures.LinkedList
 import org.domnikl.data_structures.Queue
+import org.domnikl.data_structures.Stack
 
 class Graph<T : Any> {
     private val linkedLists = mutableMapOf<T, LinkedList<T>>()
 
-    fun addEdge(x : T, y : T) {
-        val l = linkedLists.getOrPut(x) { LinkedList() }
-        l.addFirst(y)
+    fun addEdge(from: T, to: T) {
+        val l = linkedLists.getOrPut(from) { LinkedList() }
+        l.addFirst(to)
     }
 
     fun breadthFirst(source: T): List<T> {
@@ -26,6 +27,28 @@ class Graph<T : Any> {
                 if (!visited.getOrPut(it) { false }) {
                     visited[it] = true
                     queue.add(it)
+                }
+            }
+        }
+
+        return visited.filter { it.value }.map { it.key }
+    }
+
+    fun depthFirst(source: T): List<T> {
+        val visited = mutableMapOf<T,Boolean>()
+        val stack = Stack<T>()
+
+        visited[source] = true
+        stack.push(source)
+
+        while (!stack.isEmpty()) {
+            val vertex = stack.pop()
+            val connections = linkedLists[vertex]?.toList()
+
+            connections?.forEach {
+                if (!visited.getOrPut(it) { false }) {
+                    visited[it] = true
+                    stack.push(it)
                 }
             }
         }
--- src/test/kotlin/org/domnikl/algorithms/graphs/GraphTest.kt
+++ src/test/kotlin/org/domnikl/algorithms/graphs/GraphTest.kt
@@ -9,11 +9,28 @@ class GraphTest {
         val graph = Graph<Int>()
 
         graph.addEdge(0, 1)
+        graph.addEdge(0, 3)
+        graph.addEdge(0, 4)
         graph.addEdge(1, 2)
         graph.addEdge(3, 4)
+        graph.addEdge(5, 6)
 
         val visited = graph.breadthFirst(0)
 
-        assertEquals(listOf(0, 1, 2), visited)
+        assertEquals(listOf(0, 4, 3, 1, 2), visited)
+    }
+
+    @Test
+    fun `can search depth first`() {
+        val graph = Graph<String>()
+
+        graph.addEdge("A", "B")
+        graph.addEdge("B", "C")
+        graph.addEdge("D", "E")
+        graph.addEdge("A", "E")
+
+        val visited = graph.depthFirst("A")
+
+        assertEquals(listOf("A", "E", "B", "C"), visited)
     }
 }
