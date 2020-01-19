4e93df87be3c5213a2ae41272b16663e265ec800
Dominik Liebler <liebler.dominik@gmail.com>
2020-01-19 13:22:21 +0100

    refactor bucket sort

--- src/main/kotlin/org/domnikl/algorithms/sorting/BucketSort.kt
+++ src/main/kotlin/org/domnikl/algorithms/sorting/BucketSort.kt
@@ -1,52 +1,28 @@
 package org.domnikl.algorithms.sorting
 
 import kotlin.math.ceil
-import kotlin.math.floor
 
-fun <T : Comparable<T>> Array<T>.bucketSort(bucketSize: Int = 2): Array<T> {
+fun <T : Comparable<T>> Array<T>.bucketSort(bucketSize: Int = 8): Array<T> {
     val k = ceil(this.size / bucketSize.toDouble()).toInt()
-    val max = this.max()
-
-    val buckets = arrayOfNulls<MutableList<T?>>(k)
+    val max = this.max().hashCode()
+    val buckets = Array(k) { mutableListOf<T>() }
 
     this.indices.forEach { i ->
-        val bucketKey = kotlin.math.max(0, bucketKey(k, this[i], max) - 1)
+        val key = kotlin.math.max(0, k * this[i].hashCode() / max - 1)
 
-        if (buckets[bucketKey] == null) {
-            buckets[bucketKey] = mutableListOf()
-        }
-
-        buckets[bucketKey]?.add(this[i])
+        buckets[key].add(this[i])
     }
 
     var i = 0
 
-    buckets.filterNotNull().map { e ->
-        e.filterNotNull().sortedBy { it }
-    }.forEach {
-        it.forEach { e ->
-            this[i] = e
-            i++
+    buckets
+        .map { e -> e.sortedBy { it }}
+        .forEach {
+            it.forEach { e ->
+                this[i] = e
+                i++
+            }
         }
-    }
 
     return this
-}
-
-private fun <T> Array<out T>.max(): T? {
-    if (this.isEmpty()) {
-        return null
-    }
-
-    return when (this[0]) {
-        is Number -> this.maxBy { (it as Number).toDouble() }
-        else -> this.maxBy { it.hashCode() }
-    }
-}
-
-private fun <T> bucketKey(k: Int, e: T, max: T): Int {
-    return when (e) {
-        is Number -> floor(k * e.toDouble() / (max as Number).toDouble()).toInt()
-        else -> k * e.hashCode() / max.hashCode()
-    }
 }
