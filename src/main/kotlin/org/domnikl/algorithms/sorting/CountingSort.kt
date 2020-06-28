2af9adcff5fc37bdcda4c4bb98edc088f1dbd24d
HartmannS <stephan.hartmann1@googlemail.com>
2020-06-28 13:12:52 +0200

    countingsort empty array test

--- src/main/kotlin/org/domnikl/algorithms/sorting/CountingSort.kt
+++ src/main/kotlin/org/domnikl/algorithms/sorting/CountingSort.kt
@@ -1,16 +1,17 @@
 package org.domnikl.algorithms.sorting
 
 fun IntArray.countingSort(): IntArray {
+    if (this.isEmpty()) return this
     val min = this.min()!!
     val max = this.max()!!
     val counts = IntArray(max - min + 1)
 
-    this.forEach { counts[it-min]++ }
+    this.forEach { counts[it - min]++ }
 
     var pointer = 0
     counts.forEachIndexed { index, i ->
-        repeat(i){
-            this[pointer] = index+min
+        repeat(i) {
+            this[pointer] = index + min
             pointer++
         }
     }
--- src/test/kotlin/org/domnikl/algorithms/sorting/CountingSortTest.kt
+++ src/test/kotlin/org/domnikl/algorithms/sorting/CountingSortTest.kt
@@ -5,6 +5,13 @@ import org.junit.Test
 
 class CountingSortTest {
     @Test
+    fun `does not throw Exception with empty array`() {
+        val x = intArrayOf()
+
+        assertArrayEquals(x, x.countingSort())
+    }
+
+    @Test
     fun `returns copy of already sorted array`() {
         val x = intArrayOf(1, 2, 3)
 
